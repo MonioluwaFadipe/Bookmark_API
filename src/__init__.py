284d69d7f7cb93da235318592064c71f7bdad391
Fadipe Daniel <fadipemonioluwa@gmail.com>
2022-08-26 10:35:55 +0100

    Set up token authentication

--- src/__init__.py
+++ src/__init__.py
@@ -4,6 +4,7 @@ import os
 from src.auth import auth
 from src.bookmarks import bookmarks
 from src.database import db
+from flask_jwt_extended import JWTManager
 
 
 #create setups and configurations
@@ -16,14 +17,19 @@ def create_app(test_config=None):
         app.config.from_mapping(
             SECRET_KEY=os.environ.get("SECRET_KEY"),
             SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DB_URI"),
-            SQLALCHEMY_TRACK_MODIFICATIONS=False
+            SQLALCHEMY_TRACK_MODIFICATIONS=False,
+            JWT_SECRET_KEY=os.environ.get('JWT_SECRET_KEY')
         )
     else:
         app.config.from_mapping(test_config)
 
-    
+    #set up database
     db.app=app
     db.init_app(app)
+
+    #set up JWT
+    JWTManager(app)
+
     #register blueprints
     app.register_blueprint(auth)
     app.register_blueprint(bookmarks)
