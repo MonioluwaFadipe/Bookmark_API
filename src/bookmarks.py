edabd43a1c57ba580de2e606d1d024947cf0d54e
Fadipe Daniel <fadipemonioluwa@gmail.com>
2022-08-28 18:35:37 +0100

    Delete a page

--- src/bookmarks.py
+++ src/bookmarks.py
@@ -1,7 +1,7 @@
 #import module(s)
 #from crypt import methods
 from flask import Blueprint, request, jsonify
-from .constants.http_status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND, HTTP_409_CONFLICT
+from .constants.http_status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_204_NO_CONTENT, HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND, HTTP_409_CONFLICT
 import validators
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from src.database import Bookmark, db
@@ -74,7 +74,7 @@ def handle_bookmarks():
 
 #get a single page
 @bookmarks.get("/<int:id>")
-@jwt_required
+@jwt_required()
 def get_bookmark(id):
     current_user = get_jwt_identity()
 
@@ -127,3 +127,18 @@ def edit_bookmark(id):
         }), HTTP_200_OK
 
 
+#delete page
+@bookmarks.delete("/<int:id>")
+@jwt_required()
+def delete_bookmark(id):
+    current_user = get_jwt_identity()
+
+    bookmark = Bookmark.query.filter_by(user_id=current_user, id=id).first()
+
+    if not bookmark:
+        return jsonify({'message': 'item not found'}), HTTP_404_NOT_FOUND
+
+    db.session.delete(bookmark)
+    db.session.commit()
+
+    return jsonify({}), HTTP_204_NO_CONTENT
